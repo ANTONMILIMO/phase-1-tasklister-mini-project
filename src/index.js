d88be19b6c5967931cbe939db7aac597652936eb
Antony Milimo <antonymilimo2017@gmail.com>
2025-04-21 10:42:40 +0300

    added an event to the form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form');
+  const userTasks = document.getElementById('tasks');
+  taskForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const newTask = document.getElementById('new-task-description').value;
+
+    if (newTask.trim() !== '') {
+      const li = document.createElement('li');
+      li.textContent = newTask;
+      userTasks.appendChild(li);
+      taskForm.reset();
+    }
+  });
 });
